bf9479c867486c35fdec9db934fc6135a1cf8e09
Plamen <vanBasten09@abv.bg>
2018-02-09 16:36:22 +0200

    Ajax and JSON examples added

--- docs/Module5/js/ajax-utils.js
+++ docs/Module5/js/ajax-utils.js
@@ -3,13 +3,12 @@
 // Set up a namespace for our utility
 var ajaxUtils = {};
 
-
 // Returns an HTTP request object
 function getRequestObject() {
-  if (global.XMLHttpRequest) {
+  if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
-  else if (global.ActiveXObject) {
+  else if (window.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
@@ -64,3 +63,4 @@ global.$ajaxUtils = ajaxUtils;
 
 
 })(window);
+
--- docs/Module5/js/script.js
+++ docs/Module5/js/script.js
@@ -1,29 +1,66 @@
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
-    
+
+    //Datepicker
+    $( function() {
+          $( "#datepicker" ).datepicker({
+              dateFormat: "dd-mm-yy",
+              changeYear: true,
+              changeMonth: true
+            });
+        });
+
     // Unobtrusive event binding
     document.querySelector("button")
       .addEventListener("click", function () {
         
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("data/name.json", 
+         // .sendGetRequest("data/name.json", 
+         .sendGetRequest("https://apexea.oracle.com/pls/apex/gamma_dev/raspberry/get_measures/" + $("#datepicker").val() , //13-01-2018",
             function (res) {
-              var message = 
-                res.first_name + " " + res.last_name;
-              message += " lives in ";
-              message += res.address + " ";
-              message += res.postal_code + ".";
 
               console.log(res);
 
-              var message1 = 
-                res.sections[1].name + ", type " + res.sections[1].type;  
+              var_content = "";
+
+              for (var item in res.items) {
+                
+                var message = "<div class = \"col-sm-4\"><ul class=\"list-group\">";
+
+  /*                res.first_name + " " + res.last_name;
+                message += " lives in ";
+                message += res.address + " ";
+                message += res.postal_code + ".";*/
+
+                message = message + "<li class=\"list-group-item\"><span class=\"badge\">" 
+                                  + res.items[item].time + "</span>"  
+                                  + "Time"
+                                  + "</li>"
+
+                message = message + "<li class=\"list-group-item\"><span class=\"badge\">" 
+                                  + res.items[item].temp + "</span>"  
+                                  + "Temperature"
+                                  + "</li>"
+
+                message = message + "<li class=\"list-group-item\"><span class=\"badge\">" 
+                                  + res.items[item].humidity + "</span>"  
+                                  + "Humidity"
+                                  + "</li>"                  
+                  
+                /* "<p>Time: " + res.items[item].time + "</p>"; 
+                message = message + "<p>Temp: " + res.items[item].temp + "</p>" ;
+                message = message + "<p>Humidity: " + res.items[item].humidity + "</p>" ;*/
+
+                message = message + "</ul></div>";
+             /*     res.sections[1].name + ", type " + res.sections[1].type; */ 
+                var_content = var_content + message;
+
+               } 
 
               document.querySelector("#content")
-                .innerHTML =  "<h2>" + message + "</h2>" +
-                              "<h3>" + message1 + "</h3>";    
+                .innerHTML = var_content;    
 
             });
       });
